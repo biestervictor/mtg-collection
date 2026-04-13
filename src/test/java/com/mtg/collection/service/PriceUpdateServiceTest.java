b3d6df06eb4f8ce6e5b32a63cec9b91cfbac5ec7
Victor Biester <victor@biester.de>
2026-04-13 14:47:23 +0000

    fix: add missing Collections import to tests

--- src/test/java/com/mtg/collection/service/PriceUpdateServiceTest.java
+++ src/test/java/com/mtg/collection/service/PriceUpdateServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
